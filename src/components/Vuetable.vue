7336767cea366490d471e1b203fc580b6de07324
Manuel Yang <myang@nativo.com>
2021-10-15 15:40:28 -0700

    Revert "Subsitute the name of the component so it is just the name of the component not the rest of the string added to the front of it"
    This reverts commit 52430db46c637fd02b3260994988e83171e80a33.

--- src/components/Vuetable.vue
+++ src/components/Vuetable.vue
@@ -63,7 +63,7 @@
                 <template v-if="field.visible">
                   <template v-if="isFieldComponent(field.name)">
                     <component
-                      :is="field.name.substr(25)"
+                      :is="field.name"
                       :key="fieldIndex"
                       :row-data="item"
                       :row-index="itemIndex"
